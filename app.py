f55a3793dc5331cde1709dde5965279af9b8be81
李晨光 <2985827239@qq.com>
2026-03-06 14:27:22 +0800

    1

--- app.py
+++ app.py
@@ -692,4 +692,5 @@ def main() -> None:
     
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8000))  # 本地调试用默认端口
+    app.run(host="0.0.0.0", port=port, debug=True)
